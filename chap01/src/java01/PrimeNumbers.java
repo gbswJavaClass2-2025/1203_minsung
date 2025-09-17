88aa55ee359d5ee6cf563d382056927eb3a88098
kim-minseong <redjoy09@gmail.com>
2025-09-18 00:05:57 +0900

    asdf

--- chap01/src/java01/PrimeNumbers.java
+++ chap01/src/java01/PrimeNumbers.java
@@ -1,9 +1,23 @@
 
+
+import java.util.Scanner;
+
 public class PrimeNumbers {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 
+		for (int i = 2; i <= n; i++) {
+			int j;
+			for (j = 2; j <= i / 2; j++) {
+				if (i % j == 0) {
+					break;
+				}
+			}
+			if (j > i / 2) {
+				System.out.print(i + " ");
+			}
+		}
+		sc.close();
 	}
-
 }
--- chap02/src/sec02/exam03/LongExample.java
+++ chap02/src/sec02/exam03/LongExample.java
@@ -6,7 +6,7 @@ public class LongExample {
 		long var1 = 10;
 		long var2 = 20L;
 		//long var3 = 1000000000000;
-		long var4 = 1000000000000;
+		long var4 = 1000000000;
 		
 		System.out.println(var1);
 		System.out.println(var2);
